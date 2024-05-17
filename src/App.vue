18b2590af18e49b60a8d56176678da2e563ebd41
Daniel Moya <extern.daniel.moya@europcar.com>
2024-05-17 20:59:08 +0200

    ✨ Update BaseModal component with showcase

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,9 @@
 </template>
 
 <style lang="scss">
-body {
-	background: black;
-}
-
-#app {
+body, #app {
 	background: var(--color-secondary);
 	color: var(--color-secondary-accent);
+	min-width: 100dvw;
 }
 </style>
